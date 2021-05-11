24d3d0e4ea66a06144b5e37b34e855dc91411ea8
ethan <ethan.l@shopperplus.com>
2021-05-11 17:09:41 +0800

    P19

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,11 +12,13 @@ class UserController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:username, :password))
+    # @user = User.new(params.require(:user).permit(:username, :password))
+    @user = User.new(username: params[:username], password: params[:password])
     if @user.save
       flash[:notice] = "注册成功，请登录"
       redirect_to new_session_path
     else
+      # render :'user/new'
       render action: :new
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  post 'user/new', to: 'user#create'
   resources :user
   delete 'session/destroy', to: 'session#destroy' #, as: 'ddd'   路径会变成ddd_path
   resources :session
+  namespace :admin do
+    root 'users#index'
+    resources :users do
+      collection do
+        get :search
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
