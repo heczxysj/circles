f6e2cc6e8e3278b9ba1c826a040689234913f427
ethan <ethan.l@shopperplus.com>
2021-05-14 16:55:15 +0800

    Capistrano::Puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "circles"
+set :application, "my_circles"
 set :repo_url, "git@github.com:heczxysj/circles.git"
 
 # Default branch is :master
