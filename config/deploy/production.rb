2955605b088c4a572b00a8ecc04d42cfe956d5ed
ethan <ethan.l@shopperplus.com>
2021-05-18 11:46:34 +0800

    puma_bind

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ set :deploy_to, '/var/www/ethan/deploy/circles'     #部署的位置
 # PUMA
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix:///tmp/example.sock"      #根据nginx配置链接的sock进行设置，需要唯一
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"      #根据nginx配置链接的sock进行设置，需要唯一
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
