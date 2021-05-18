6d840d81bbba9a1ccd041df0802303c6fca2bfc7
ethan <ethan.l@shopperplus.com>
2021-05-18 13:36:08 +0800

    delete set puma

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,18 +5,18 @@ server '192.168.50.250', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :deploy_to, '/var/www/ethan/deploy/circles'     #部署的位置
 # set :deploy_to, '/Users/ethan/deploy'     #部署的位置
 # PUMA
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"      #根据nginx配置链接的sock进行设置，需要唯一
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_init_active_record, false
-set :puma_preload_app, true
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"      #根据nginx配置链接的sock进行设置，需要唯一
+# set :puma_conf, "#{shared_path}/puma.rb"
+# set :puma_access_log, "#{shared_path}/log/puma_error.log"
+# set :puma_error_log, "#{shared_path}/log/puma_access.log"
+# set :puma_role, :app
+# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_threads, [0, 16]
+# set :puma_workers, 0
+# set :puma_init_active_record, false
+# set :puma_preload_app, true
 set :ssh_options, {
   keys: %w(/Users/ethan/bycbd_ubuntu.pem),  #本机链接服务器的证书
   forward_agent: false
